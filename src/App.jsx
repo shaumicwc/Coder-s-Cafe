3d5af208365aa745409b5470e45c265bd0edb8b2
shaumicwc <112300736+shaumicwc@users.noreply.github.com>
2023-03-31 11:28:42 +0600

    react toast added

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,25 @@ import './App.css'
 import Header from './Components/Header/Header'
 import Faq from './Components/FAQ/Faq'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { toast, ToastContainer } from 'react-toastify';
 import Blogs from './Components/Blogs/Blogs';
-
-
 
 function App() {
   const [readTimes, setReadTime] = useState(0);
+  const [bookmarks, setBookmark] = useState([]);
 
   const handleReadTime = (readTime) =>{
       const readTimeInt = parseInt(readTime);
       setReadTime(readTimes + readTimeInt);
+  }
 
+  const handleBookmark = (bookmark) => {
+    if (bookmarks.includes(bookmark)) {
+      toast('This bookmark is already added');
+      return;
+    }
+    const newBookmark = [...bookmarks, bookmark];
+    setBookmark(newBookmark);
   }
 
   return (
@@ -23,8 +31,9 @@ function App() {
       <Blogs 
       readTimes={readTimes}
       handleReadTime={handleReadTime}
+      handleBookmark={handleBookmark}
       ></Blogs>
-      
+      <ToastContainer></ToastContainer>
       <Faq></Faq>
 
 
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -4,29 +4,31 @@ import SideCard from '../SideCard/SideCard';
 import './Blogs.css'
 
 
-const Blogs = ({handleReadTime, readTimes}) => {
+const Blogs = ({ handleReadTime, readTimes }) => {
     const [blogs, setBlog] = useState([]);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('data.json')
-        .then(res=> res.json())
-        .then(data => setBlog(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setBlog(data))
+    }, [])
     return (
         <div className='blog-container row'>
             <div className='blog-card-container col-md-8'>
                 {
                     blogs.map(blog => <BlogCard
-                    blog={blog}
-                    key={blog.id}
-                    handleReadTime={handleReadTime}
+                        blog={blog}
+                        key={blog.id}
+                        handleReadTime={handleReadTime}
                     ></BlogCard>)
                 }
             </div>
             <div className='bookmark-cart col-md-4 card border-0'>
-                    <SideCard
-                    readTimes={readTimes}
-                    ></SideCard>
+             
+                <SideCard
+                   readTimes={readTimes}
+                   handleReadTime={handleReadTime}
+                ></SideCard>
             </div>
         </div>
     );
