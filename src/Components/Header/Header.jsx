87972c80661731e41cc9c8b10878c255178e2a7d
shaumicwc <112300736+shaumicwc@users.noreply.github.com>
2023-03-31 18:15:21 +0600

    image code fixed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
             <h6 className='nav-title'>Knowledge Cafe</h6>
           
             <div>
-                <img className='header-img w-75' src='/src/images/avatar.png' alt="" />
+                <img className='header-img w-75' src='./src/images/avatar.png' alt="" />
             </div>
         </nav>
         <hr />
