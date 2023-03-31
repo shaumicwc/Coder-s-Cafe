40d7dd5778c00dedff6f58215ff648f213f5f8eb
shaumicwc <112300736+shaumicwc@users.noreply.github.com>
2023-03-31 18:32:09 +0600

    code updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
     return (
         <>
         <nav className='header-container'>
-            <h6 className='nav-title'>Knowledge Cafe</h6>
+            <h6 className='nav-title'>Coders Cafe</h6>
           
             <div>
-                <img className='header-img w-75' src='./src/images/avatar.png' alt="" />
+            <img src="https://images.unsplash.com/photo-1530268729831-4b0b9e170218?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" className='header-img' />
             </div>
         </nav>
         <hr />
